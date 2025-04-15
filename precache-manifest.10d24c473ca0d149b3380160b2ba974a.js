7f5cc4ba7eb2f8c8b5fd2065715da52589e63a36
Jaganath Panda <jaganathpanda@Jaganaths-MacBook-Air.local>
2025-04-15 23:33:19 +0530

    Updates

---
+++ precache-manifest.10d24c473ca0d149b3380160b2ba974a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a5ea774374d0e34bbb5736e79386d5",
+    "url": "/aemreact/index.html"
+  },
+  {
+    "revision": "3930497dd46f9da8779c",
+    "url": "/aemreact/static/css/main.e9feee97.chunk.css"
+  },
+  {
+    "revision": "b3b93c1c0b05f18a8b47",
+    "url": "/aemreact/static/js/2.e468c016.chunk.js"
+  },
+  {
+    "revision": "4215145f8de1c86dce04f91f2e04b52d",
+    "url": "/aemreact/static/js/2.e468c016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3930497dd46f9da8779c",
+    "url": "/aemreact/static/js/main.076b711d.chunk.js"
+  },
+  {
+    "revision": "da38f23c0f6b7abd5943",
+    "url": "/aemreact/static/js/runtime-main.30e67fd2.js"
+  },
+  {
+    "revision": "19a5bdabd660fde16554c866e650eadc",
+    "url": "/aemreact/static/media/icon-back.19a5bdab.svg"
+  }
+]);
